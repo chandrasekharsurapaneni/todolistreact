fd27c657145bf3fda5e870f916b68209b172652b
chandrasekharsurapaneni <chandusurapaneni30@gmail.com>
2025-04-19 22:02:31 +0530

    iscomplete

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+
 function App() {
   const [items, setItems] = useState([]);
   const [title, setTitle] = useState("");
-  const [isEditable, setIsEditable] = useState({});
-  const [isCompleted, setIsCompleted] = useState(false);
+  const [isEditable,setIsEditable]=useState({});
+
 
   async function handleAdd() {
     if (!title.trim()) return;
@@ -19,48 +20,42 @@ function App() {
     getTasks();
   }
 
-  async function handleEdit(item, value) {
-    console.log(typeof value == "boolean");
-    await axios.put(
-      `https://todolist-latest-lbz4.onrender.com/api/todos/${item.id}`,
-      {
-        title: typeof value == "boolean" ? item.title : value,
-        completed: typeof value == "boolean" ? !item.completed : item.completed,
-      }
-    );
+  async function handleEdit(item,value){
+    console.log(typeof(value)=="boolean");
+    await axios.put(`https://todolist-latest-lbz4.onrender.com/api/todos/${item.id}`, {
+      
+      
+      title:typeof(value)=="boolean"?item.title:value ,
+      completed:typeof(value)=="boolean"? !item.completed:item.completed,
+    });
     setTitle("");
     getTasks();
   }
 
-  const handleInput = (e, id) => {
-    setItems(
-      items.map(item =>
-        item.id === id ? { ...item, title: e.target.value } : item
-      )
-    );
-  };
+const handleInput=(e,id)=>{
+setItems(items.map(item =>
+  item.id === id ? { ...item, title: e.target.value } : item
+))
+}
 
-  const handleDisable = id => {
-    setIsEditable(prev => ({ ...prev, [id]: !prev[id] }));
-  };
+  const handleDisable =(id)=>{
+setIsEditable((prev)=>({...prev,[id]:!prev[id]}))
+  }
 
   async function handleDelete(id) {
-    await axios.delete(
-      `https://todolist-latest-lbz4.onrender.com/api/todos/${id}`
-    );
+    await axios.delete(`https://todolist-latest-lbz4.onrender.com/api/todos/${id}`);
     getTasks();
   }
-  const handleEnter = (e, item) => {
-    if (e.key === "Enter") {
-      handleEdit(item, e.target.value);
-      handleDisable(item.id);
-    }
-  };
+  const handleEnter=(e,item)=>{
+      if(e.key==='Enter'){
+        handleEdit(item,e.target.value)
+        handleDisable(item.id)
+      }
+      
+  }
   async function getTasks() {
     try {
-      const response = await axios.get(
-        "https://todolist-latest-lbz4.onrender.com/api/todos"
-      );
+      const response = await axios.get("https://todolist-latest-lbz4.onrender.com/api/todos");
       setItems(response.data);
     } catch (e) {
       console.log("something went wrong or loading");
@@ -74,32 +69,21 @@ function App() {
   return (
     <div className="App vh-100 p-0 ">
       <div className="d-flex flex-column align-items-center justify-content-center vh-100">
-        <h1 className="mb-4" style={{ color: "#0ff" }}>
-          To Do List
-        </h1>
+        <h1 className="mb-4" style={{color:"#0ff"}} >To Do List</h1>
         <Container className="container-box " style={{ maxWidth: "500px" }}>
           {items.map(item => (
             <Row key={item.id}>
               <Col>
-                <input
-                  type="checkbox"
-                  checked={item.completed}
-                  onChange={() => handleEdit(item, item.completed)}
-                />
+              <input type="checkbox" checked={item.completed} onChange={()=>handleEdit(item,item.completed)} />
                 <input
                   type="text"
                   value={item.title}
                   disabled={!isEditable[item.id]}
-                  onKeyDown={e => handleEnter(e, item)}
-                  onChange={e => handleInput(e, item.id)}
-                  className={`my-2 task-input p-2 w-75 rounded m-3 ${
-                    item.completed ? "text-decoration-line-through" : ""
-                  }`}
+                  onKeyDown={(e)=>handleEnter(e,item)}
+                  onChange={(e)=>handleInput(e,item.id)}
+                  className={`my-2 task-input p-2 w-75 rounded m-3 ${item.completed? "text-decoration-line-through":"" }`}
                 />
-                <i
-                  className="bi bi-pencil-square"
-                  onClick={() => handleDisable(item.id)}
-                ></i>
+                <i className="bi bi-pencil-square" onClick={()=>handleDisable(item.id)}></i>
                 <i
                   className="bi bi-trash-fill trash-icon m-3"
                   onClick={() => handleDelete(item.id)}
@@ -107,7 +91,7 @@ function App() {
               </Col>
             </Row>
           ))}
-          <Row>
+          <Row >
             <Col>
               <input
                 type="text"
